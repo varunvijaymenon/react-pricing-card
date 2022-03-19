fe56160b89386159d994361c3ca449b3a47c4776
varun vijay <varunvijaymenon@gmail.com>
2022-03-19 19:06:58 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Card from "./components/Card";
 import './App.css';
 
 function App() {
+  const data1 = [['Single User', '5GB Storage', 'Unlimited Public Projects', 'Community Access'], 
+  ['5 Users', '50GB Storage', 'Unlimited Public Projects', 'Community Access', 'Unlimited Private Projects', 'Dedicated Phone Support', 'Free Subdomain'],
+  ['Unlimited Users', '150GB Storage', 'Unlimited Public Projects', 'Community Access', 'Unlimited Private Projects', 'Dedicated Phone Support', 'Unlimited Free Subdomains', 
+  'Monthly Status Reports']];
+
+  const data2 = [['Unlimited Private Projects', 'Dedicated Phone Support', 'Free Subdomain', 'Monthly Status Reports'],
+  ['Monthly Status Reports'],[]]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <div className="Card-container">
+    <Card name="FREE" price="0"  data1= {data1[0]} data2={data2[0]}/>
+    <Card name="PLUS" price="9"  data1= {data1[1]} data2={data2[1]}/>
+    <Card name="PRO"  price="49" data1= {data1[2]} data2={data2[2]}/>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardHeader, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+import "./Card.css"
+
+export default function Card(props) {
+  const data_render1 = props.data1.map((e) => <li><i class="fas fa-check"></i>&nbsp;&nbsp;{e}</li>);
+  const data_render2 = props.data2.map((d) => <li className="muted-text"><i class="fas fa-times"></i>&nbsp;&nbsp;{d}</li>);
+  return (
+    <MDBCard alignment='center' className="card-container" style={{ width: '25rem'}}>
+      <div className="overall-container">
+      <MDBCardHeader><div><b style={{ color: 'grey' }}>{props.name}</b></div>
+      <h2 className="pack-price">${props.price}</h2><sub><b >/month</b></sub>
+      </MDBCardHeader>
+      <MDBCardBody className="card-body" style={{padding: '0rem'}}>
+        {/* <MDBCardTitle>Special title treatment</MDBCardTitle> */}
+          {data_render1}
+          {data_render2}
+        {/* <MDBCardText>With supporting text below as a natural lead-in to additional content.</MDBCardText> */}
+        <MDBBtn rounded className="button"><b>Button</b></MDBBtn>
+      </MDBCardBody>
+      </div>
+    </MDBCard>
+    
+  );
+}
